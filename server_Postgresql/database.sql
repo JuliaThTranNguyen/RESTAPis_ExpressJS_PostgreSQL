a5d53cd81fc069c55bf1e4b1fde79faa699b5e1a
Thuy Hien Tran Nguyen <trhien541@gmail.com>
2023-02-18 02:50:21 +0200

    Update database.sql

--- server_Postgresql/database.sql
+++ server_Postgresql/database.sql
@@ -11,7 +11,7 @@ CREATE TABLE Todo (
   status mood NOT NULL
 );
 
-CREATE TABLE User (
+CREATE TABLE User_IN (
   id SERIAL PRIMARY KEY,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
