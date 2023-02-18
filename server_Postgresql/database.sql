83355b8397661f3148471ec496da0717bb025917
Thuy Hien Tran Nguyen <trhien541@gmail.com>
2023-02-18 02:59:53 +0200

    Update database.sql

--- server_Postgresql/database.sql
+++ server_Postgresql/database.sql
@@ -2,17 +2,20 @@ CREATE DATABASE todo_item;
 
 CREATE TYPE mood AS ENUM ('NotStarted', 'OnGoing', 'Completed');
 CREATE TABLE Todo (
-  id SERIAL PRIMARY KEY,
+  id INTEGER NOT NULL,
   name VARCHAR(255) NOT NULL,
   description VARCHAR(255),
   user_id INTEGER NOT NULL,
   created_at TIMESTAMP NOT NULL,
   updated_at TIMESTAMP NOT NULL,
-  status mood NOT NULL
+  status mood NOT NULL,
+  PRIMARY KEY (id, user_id),
+  FOREIGN KEY (user_id)
+      REFERENCES User_IN (user_id)
 );
 
 CREATE TABLE User_IN (
-  id SERIAL PRIMARY KEY,
+  user_id SERIAL PRIMARY KEY,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
   created_at TIMESTAMP NOT NULL,
